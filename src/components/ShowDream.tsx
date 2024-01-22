b1bb715ba8197bffc732d6fc28a60f4470b72eff
Jessica Tell <hello.jessicatell@gmail.com>
2024-01-22 13:56:31 +0100

    Made a class and started to implement it

--- src/components/ShowDream.tsx
+++ src/components/ShowDream.tsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import { Dream } from "../models/Dream";
+
 //underkomponent till DreamlogApp.tsx
 export const ShowDream = () => {
+  const [dream, setDream] = useState(new Dream(1, " Tågluff i Europa", false));
+
+  const handleClick = () => {
+    setDream({ ...dream, isfulFilled: false });
+  };
+
+  const handleCheckboxChange = () => {
+    setDream({ ...dream, isfulFilled: !dream.isfulFilled });
+  };
+
   return (
     <>
       <h2>Dreams</h2>
+      <h4>
+        <input
+          type="checkbox"
+          checked={dream.isfulFilled}
+          onChange={handleCheckboxChange}
+        />
+        {dream.name}{" "}
+      </h4>
+      <button onClick={handleClick}>Remove Dream</button>
+      <button>Add New Dream</button>
     </>
   );
 };
---
+++ src/models/Dream.ts
@@ -0,0 +1,7 @@
+export class Dream {
+  constructor(
+    public id: number,
+    public name: string,
+    public isfulFilled: boolean
+  ) {}
+}
